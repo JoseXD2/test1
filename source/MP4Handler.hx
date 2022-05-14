85afec033f20d2e0ad7aa71cca77abec47e5268f
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-05-14 13:00:01 -0300

    Update MP4Handler.hx

--- source/MP4Handler.hx
+++ source/MP4Handler.hx
@@ -66,7 +66,7 @@ class MP4Handler
 		#else
 		finishCallback = callback;
 
-		vlcBitmap = new VlcBitmap();
+		var vlcBitmap:vclBitmap = new VlcBitmap();
 		vlcBitmap.set_height(FlxG.stage.stageHeight);
 		vlcBitmap.set_width(FlxG.stage.stageHeight * (16 / 9));
 
